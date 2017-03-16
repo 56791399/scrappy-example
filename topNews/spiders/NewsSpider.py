93e82f5d32975fe4fe710aef00c72253928995bd
larce <luis.arce22@gmail.com>
2017-03-16 19:32:18 -0400

    Updated script to get some information and save it in a file

---
+++ topNews/spiders/NewsSpider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+# //div[@class='ab-column column']//div[@class='collection']//h2[@class='story-heading']
+class NewsSpider(scrapy.Spider):
+    name = "news"
+
+    def start_requests(self):
+        urls = [
+            'https://www.nytimes.com'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        data = str(response.xpath("//div[@class='ab-column column']//div[@class='collection']//h2[@class='story-heading']"))
+        filename = 'news-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(data)
+        self.log('Saved file %s' % filename)
