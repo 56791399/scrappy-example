7d007992125860d7e2b2ca61b6f807cb028d359d
larce <luis.arce22@gmail.com>
2017-04-10 22:49:29 -0400

    Adding another spider

---
+++ topNews/spiders/NewsSpider3.py
@@ -0,0 +1,52 @@
+
+import scrapy
+import requests
+import json
+from scrapy.selector import Selector
+
+class NewsSpider3(scrapy.Spider):
+    name = "news3"
+
+    def start_requests(self):
+        urls = [
+            'http://www.lostiempos.com/'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        dataArray = response.xpath("//div[@class='term-1']").extract()
+        toPrint = ""
+        for body in dataArray:
+            try:
+                newsImg = Selector(text=body).xpath("//img/@src").extract() # Link to new
+                newsImg0 = "noImage"
+                if len(newsImg) > 0:
+                    newsImg0 = str(newsImg[0].replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201c", "E").replace(u"\xed", "i").replace(u"\xf3", "o").replace(u"\xf1", "n").replace(u"\xe1", "a").encode('utf8'))
+
+                newsSummary = Selector(text=body).xpath("//div[@class='sin-sumario']/text()").extract() # Link to new
+                newsSummary0 = "noSummary"
+                if len(newsSummary) > 0:
+                    newsSummary0 = str(newsSummary[0].replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201c", "E").replace(u"\xed", "i").replace(u"\xf3", "o").replace(u"\xf1", "n").replace(u"\xe1", "a").encode('utf8'))
+
+                newsTitle = Selector(text=body).xpath("//div[@class='views-field-title']//a/text()").extract() # Link to new
+                newsTitle0 = "noTitle"
+                if len(newsTitle) > 0:
+                    newsTitle0 = str(newsTitle[0].replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201c", "E").replace(u"\xed", "i").replace(u"\xf3", "o").replace(u"\xf1", "n").replace(u"\xe1", "a").encode('utf8'))
+
+                headers = {'Content-type': 'application/json'}
+                if len(newsTitle) > 0 and (len(newsSummary) > 0 or len(newsContainLi) > 0):
+                    newsSummary0 = str(newsSummary[0].replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201c", "E").replace(u"\xed", "i").replace(u"\xf3", "o").replace(u"\xf1", "n").replace(u"\xe1", "a").encode('utf8'))
+                    data = {"title": newsTitle0, "body": newsSummary0, "likes": '2', "imgUrl": newsImg0}
+                data_json = json.dumps(data)
+                url = "http://localhost:9000/v1/news"
+                responsePost = requests.post(url, data=data_json, headers=headers)
+            except Exception as e:
+                print(str(e))
+                print("Exception in the scrapping")
+            else:
+                pass
+            finally:
+                pass
+        self.log('Spider 3 run')
